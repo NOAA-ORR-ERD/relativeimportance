3d11b6a3381cdc164914789c8a345ce3889eda26
Alexander Miranda <alex.miranda@noaa.gov>
2015-07-14 11:27:19 -0700

    Scoping issues have been resolved and updated lint errors

--- relativeImportance.js
+++ relativeImportance.js
@@ -1,130 +1,116 @@
 $(document).ready(function(){
     var Triangle = (function(selector, opts){
-        var canvasHTML = document.getElementById(selector);
-        var ctx = canvasHTML.getContext('2d');
-        var rect = canvasHTML.getBoundingClientRect();
-        var offset = {top: rect.top + document.body.scrollTop, left: rect.left + document.body.scrollLeft};
-        var boxSize = canvasHTML.width;
+        this.canvasHTML = document.getElementById(selector);
+        this.ctx = this.canvasHTML.getContext('2d');
+        var rect = this.canvasHTML.getBoundingClientRect();
+        this.offset = {top: rect.top + document.body.scrollTop, left: rect.left + document.body.scrollLeft};
+        var boxSize = this.canvasHTML.width;
         var pressed = false;
         
-        canvasHTML.addEventListener('mouseup', function(){
+        this.canvasHTML.addEventListener('mouseup', function(){
             pressed = false;
         });
     
-        canvasHTML.addEventListener('mousedown', function(){
+        this.canvasHTML.addEventListener('mousedown', function(){
             pressed = true;
         });
-        canvasHTML.addEventListener('mousedown', this.draw, true);
-        canvasHTML.addEventListener('mousemove', function(ev) {
+        this.canvasHTML.addEventListener('mousedown', function(ev){
+            Triangle.prototype.draw.call(this, ev);
+        }.bind(this));
+        this.canvasHTML.addEventListener('mousemove', function(ev) {
             if (!pressed){
                 return;
             }
-            this.draw(ev);
-        });
+            Triangle.prototype.draw.call(this, ev);
+        }.bind(this));
       var _Opts = {
           sideLength: 100,
           point1Name: 'Point 1',
           point2Name: 'Point 2',
           point3Name: 'Point 3'
-      }
+      };
       
       var optsKeys = Object.keys(opts);
       
       for (var i = 0; i < optsKeys.length; i++){
-          _Opts[optsKeys[i]] = opts[optsKeys[i]];        
+          _Opts[optsKeys[i]] = opts[optsKeys[i]];
       }
         
-      var sideLength = _Opts.sideLength;
-      var boxSize = _Opts.boxSize;
+      this.sideLength = _Opts.sideLength;
       
-      var triHeight = sideLength * (Math.sqrt(3) / 2);
-      var vertTrans = (boxSize - triHeight) / 2 - offset.top;
-      var horizTrans = (boxSize - sideLength) / 2 - offset.left;
+      var triHeight = this.sideLength * (Math.sqrt(3) / 2);
+      this.vertTrans = (boxSize - triHeight) / 2 - this.offset.top;
+      var horizTrans = (boxSize - this.sideLength) / 2 - this.offset.left;
         
-      var point1 = {x: boxSize / 2 - offset.left, y: vertTrans, label: _Opts.point1Name};
-      var point2 = {x: horizTrans, y: vertTrans + triHeight, label: _Opts.point2Name};
-      var point3 = {x: horizTrans + sideLength, y: vertTrans + triHeight, label: _Opts.point3Name};
-                
-      this.getPath = function(){
-          var path = new Path2D();
-          path.moveTo(point1.x, point1.y);
-          path.lineTo(point2.x, point2.y);
-          path.lineTo(point3.x, point3.y);
-          path.lineTo(point1.x, point1.y);
-          return path;
-      }();
-       
-      this.draw = function(ev){
-          var mouseClick = this.getMousePosition(ev);
-          console.log(mouseClick);
-          var path = this.getPath;
-          var collision = ctx.isPointInPath(path, mouseClick.x, mouseClick.y);
-          if (ev === undefined || collision){
-              ctx.clearRect(0, 0, canvasHTML.width, canvasHTML.height);
-              var path = this.getPath;
-              ctx.stroke(path);
-              ctx.closePath();
-              this.drawText();
-              this.drawCircle(ev);
-          }
-      };
-        
-      this.getMousePosition = function(ev){
-          if (ev === undefined){
+      this.point1 = {x: boxSize / 2 - this.offset.left, y: this.vertTrans, label: _Opts.point1Name};
+      this.point2 = {x: horizTrans, y: this.vertTrans + triHeight, label: _Opts.point2Name};
+      this.point3 = {x: horizTrans + this.sideLength, y: this.vertTrans + triHeight, label: _Opts.point3Name};
+    });
+    
+    Triangle.prototype.textMidPoint = function(text){
+        return this.ctx.measureText(text).width / 2;
+    };
+    
+    Triangle.prototype.draw = function(ev){
+        var mouseClick = Triangle.prototype.getMousePosition.call(this, ev);
+        var path = Triangle.prototype.getPath.call(this);
+        var collision = this.ctx.isPointInPath(path, mouseClick.x, mouseClick.y);
+        if (ev === undefined || collision){
+            this.ctx.clearRect(0, 0, this.canvasHTML.width, this.canvasHTML.height);
+            this.ctx.stroke(path);
+            this.ctx.closePath();
+            this.drawText();
+            this.drawCircle(ev);
+         }
+    };
+    
+    Triangle.prototype.getMousePosition = function(ev){
+        if (ev === undefined){
               return {x: 0, y: 0};
           }
           var coords;
           if (ev['layerX'] !== undefined) {
               coords = {
-                  x: ev.pageX - offset.left,
-                  y: ev.pageY - offset.top
+                  x: ev.pageX - this.offset.left,
+                  y: ev.pageY - this.offset.top
               };
           }
           return coords;
-        }
-       
-      this.drawText = function(){
-          var scalingFact = 1 / 10;
-          var fontSize = (scalingFact * sideLength);
-          ctx.font = fontSize + "px arial";
-          ctx.fillText(point1.label, point1.x - textMidPoint(point1.label), point1.y - fontSize / 2);
-          ctx.fillText(point2.label, point2.x - textMidPoint(point2.label), point2.y + fontSize);
-          ctx.fillText(point3.label, point3.x - textMidPoint(point3.label), point3.y + fontSize);
-      };
-        
-      function textMidPoint(text){
-          return ctx.measureText(text).width / 2;
-      }
-        
-      this.drawCircle = function drawCircle(ev){
-          var coords;
-          var initialY = vertTrans + sideLength * (Math.sqrt(3) / 3);
-          if (ev === undefined){
-              coords = {x: canvasHTML.width / 2 - offset.left, y: initialY - offset.top};
-          } else {
-              coords = this.getMousePosition(ev);
-          }
-          ctx.beginPath();
-          ctx.arc(coords.x, coords.y, 5, 0, Math.PI * 2, true);
-          ctx.closePath();
-          ctx.fill();
-          return coords;
-       }
-        
-    });
-    Triangle.prototype.draw = function(){
-      var mouseClick = this.getMousePosition(ev);
-          var path = this.getPath;
-          var collision = ctx.isPointInPath(path, mouseClick.x, mouseClick.y);
-          if (ev === undefined || collision){
-              ctx.clearRect(0, 0, canvasHTML.width, canvasHTML.height);
-              var path = this.getPath;
-              ctx.stroke(path);
-              ctx.closePath();
-              this.drawText();
-              this.drawCircle(ev);
-          }      
     };
+    
+    Triangle.prototype.drawCircle = function(ev){
+        var coords;
+        var initialY = this.vertTrans + this.sideLength * (Math.sqrt(3) / 3);
+        if (ev === undefined){
+              coords = {x: this.canvasHTML.width / 2 - this.offset.left, y: initialY - this.offset.top};
+        } else {
+            coords = Triangle.prototype.getMousePosition.call(this, ev);
+        }
+        this.ctx.beginPath();
+        this.ctx.arc(coords.x, coords.y, 5, 0, Math.PI * 2, true);
+        this.ctx.closePath();
+        this.ctx.fill();
+        return coords;
+    };
+    
+    Triangle.prototype.drawText = function(){
+        var scalingFact = 1 / 10;
+        var fontSize = (scalingFact * this.sideLength);
+        this.ctx.font = fontSize + "px arial";
+        this.ctx.fillText(this.point1.label, this.point1.x - this.textMidPoint(this.point1.label), this.point1.y - fontSize / 2);
+        this.ctx.fillText(this.point2.label, this.point2.x - this.textMidPoint(this.point2.label), this.point2.y + fontSize);
+        this.ctx.fillText(this.point3.label, this.point3.x - this.textMidPoint(this.point3.label), this.point3.y + fontSize);
+    };
+    
+    Triangle.prototype.getPath = function(){
+        var path = new Path2D();
+        path.moveTo(this.point1.x, this.point1.y);
+        path.lineTo(this.point2.x, this.point2.y);
+        path.lineTo(this.point3.x, this.point3.y);
+        path.lineTo(this.point1.x, this.point1.y);
+        return path;
+    };
+    
     var triangle = new Triangle('tutorial', {sideLength: 200, point1Name: 'column',point2Name: 'surface', point3Name: 'shoreline'});
     triangle.draw();
 });
