5d155a0ee4e3dfc374fc7fd7f5c9ac2f554ea0cc
Alexander Miranda <alex.miranda@noaa.gov>
2015-07-15 13:59:26 -0700

    The percentDistances method has been updated to be more accurate in finding the correct proportions

--- relativeImportance.js
+++ relativeImportance.js
@@ -3,6 +3,7 @@ var Triangle = (function(selector, opts){
         this.ctx = this.canvasHTML.getContext('2d');
         var rect = this.canvasHTML.getBoundingClientRect();
         this.offset = {top: rect.top + document.body.scrollTop, left: rect.left + document.body.scrollLeft};
+        console.log(this.offset);
         var boxSize = this.canvasHTML.width;
         var pressed = false;
         
@@ -41,7 +42,7 @@ var Triangle = (function(selector, opts){
       this.vertTrans = (boxSize - triHeight) / 2 - this.offset.top;
       var horizTrans = (boxSize - this.sideLength) / 2 - this.offset.left;
         
-      var point1 = {x: boxSize / 2 - this.offset.left, y: this.vertTrans + this.offset.top, label: _Opts.point1Name};
+      var point1 = {x: boxSize / 2 - this.offset.left, y: this.vertTrans, label: _Opts.point1Name};
       var point2 = {x: horizTrans, y: this.vertTrans + triHeight + this.offset.top, label: _Opts.point2Name};
       var point3 = {x: horizTrans + this.sideLength, y: this.vertTrans + triHeight + this.offset.top, label: _Opts.point3Name};
     
@@ -49,8 +50,8 @@ var Triangle = (function(selector, opts){
     });
     
     Triangle.prototype.textMidPoint = function(text){
-        return this.ctx.measureText(text).width / 2;   
-    }
+        return this.ctx.measureText(text).width / 2;
+    };
     
     Triangle.prototype.draw = function(ev){
         var mouseClick = Triangle.prototype.getMousePosition.call(this, ev);
@@ -77,8 +78,8 @@ var Triangle = (function(selector, opts){
                   y: ev.pageY - this.offset.top
               };
           }
-          return coords;   
-    }
+          return coords;
+    };
     
     Triangle.prototype.drawCircle = function(ev){
         var coords;
@@ -92,8 +93,8 @@ var Triangle = (function(selector, opts){
         this.ctx.arc(coords.x, coords.y, 5, 0, Math.PI * 2, true);
         this.ctx.closePath();
         this.ctx.fill();
-        this.coords = coords;  
-    }
+        this.coords = coords;
+    };
     
     Triangle.prototype.drawText = function(){
         var scalingFact = 1 / 10;
@@ -101,9 +102,9 @@ var Triangle = (function(selector, opts){
         this.ctx.font = fontSize + "px arial";
         this.ctx.fillText(this.points[0].label, this.points[0].x - this.textMidPoint(this.points[0].label), this.points[0].y - fontSize / 2);
         for (var i = 1; i < this.points.length; i++){
-            this.ctx.fillText(this.points[i].label, this.points[i].x - this.textMidPoint(this.points[i].label), this.points[i].y + fontSize);    
-        }  
-    }
+            this.ctx.fillText(this.points[i].label, this.points[i].x - this.textMidPoint(this.points[i].label), this.points[i].y + fontSize);
+        }
+    };
     
     Triangle.prototype.getPath = function(){
         var path = new Path2D();
@@ -112,39 +113,42 @@ var Triangle = (function(selector, opts){
             path.lineTo(this.points[i].x, this.points[i].y);
         }
         path.lineTo(this.points[0].x, this.points[0].y);
-        return path;   
-    }
+        return path;
+    };
     
     Triangle.prototype.percentDistances = function(){
         var percents = {};
         var sum = 0;
         for (var key in this.distances){
             percents[key] = this.getPercent(this.distances[key]);
-            sum += percents[key];
+            sum += this.distances[key];
         }
         console.log(percents, sum);
-    }
+    };
     
     Triangle.prototype.getPercent = function(distance){
-        return Math.abs((distance / (this.sideLength * Math.sqrt(3)))) * 100;   
-    }
+        var proportion = (1 - (2 * distance / (this.distancesSum))) * 100;
+        return proportion;// * Math.sqrt(3) / 2))) * 100;   
+    };
     
     Triangle.prototype.relativeDistances = function(){
         var distances = {};
+        var sum = 0;
         for (var i = 0; i < this.points.length; i++){
             var distance = this.distanceTo(this.points[i]);
             distances[this.points[i].label] = distance;
+            sum += distance;
         }
         this.distances = distances;
-        console.log(distances);
+        this.distancesSum = sum;
         this.percentDistances();
-    }
+    };
     
     Triangle.prototype.distanceTo = function(point){
-        return Math.sqrt(Math.pow(point.x - this.coords.x, 2) + Math.pow(point.y - this.coords.y, 2));   
-    }
+        return Math.sqrt(Math.pow(point.x - this.coords.x, 2) + Math.pow(point.y - this.coords.y, 2));
+    };
 
-$(document).ready(function(){    
+$(document).ready(function(){
     var triangle = new Triangle('tutorial', {sideLength: 200, point1Name: 'column',point2Name: 'surface', point3Name: 'shoreline'});
     triangle.draw();
 });
